80253cec5942695db7cc85b7a05fca09726d88e5
Joona Lusa <joona.lusa@hotmail.com>
2021-11-25 10:35:49 -0500

    able to retrieve the weather for city that user inputted

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,23 @@ var searchList = document.querySelector(".search-history");
 
 $("#search-btn").click(function() {
 
+    var searchCity = $("#user-search").val();
     // API key: A2mD7VganlN2FLWzfesMPmEdQm8etbxpl9ApyKhFC_k
-    var geoLocationUrl = "https://geocode.search.hereapi.com/v1/geocode?q=" + $("#user-search").val() + "&apiKey=A2mD7VganlN2FLWzfesMPmEdQm8etbxpl9ApyKhFC_k";
+    var geoLocationUrl = "https://geocode.search.hereapi.com/v1/geocode?q=" + searchCity + "&apiKey=A2mD7VganlN2FLWzfesMPmEdQm8etbxpl9ApyKhFC_k";
 
-    if ($("#user-search").val() !== "") {
+    if (searchCity !== "") {
         var newBtnEl = document.createElement("button");
         newBtnEl.classList = "btn-primary btn-sm btn-block list-group-item-dark mb-2";
-        newBtnEl.textContent = $("#user-search").val();
+        newBtnEl.textContent = searchCity;
         searchList.appendChild(newBtnEl);
-        $("#user-search").val(empty);
+        $(searchCity).val(empty);
         fetch(geoLocationUrl).then(function(response) {
             if (response.ok) {
 
                 response.json().then(function(data) {
-                    getCurrentWeather(data);
+                    getCurrentWeather(searchCity, data);
 
                 })
-                return;
 
             } else {
                 alert("Something went wrong with api");
@@ -33,20 +33,29 @@ $("#search-btn").click(function() {
 
 });
 
-var getCurrentWeather = function(data) {
+var getCurrentWeather = function(search, data) {
 
     // Api key: 229ca32802b237bb05e7ab6cfdfacd0c
-    var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.items[0].position.lat + "&lon=" + (data.items[0].position.lng) + "&appid=229ca32802b237bb05e7ab6cfdfacd0c";
+    var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.items[0].position.lat + "&lon=" + (data.items[0].position.lng) + "&units=metric&appid=229ca32802b237bb05e7ab6cfdfacd0c";
     fetch(weatherApiUrl).then(function(response) {
         if (response.ok) {
 
             response.json().then(function(data) {
                 console.log(data.current);
+                console.log(search);
+                console.log(data.current.temp);
+                console.log(data.current.wind_speed);
+                console.log(data.current.humidity);
+                console.log(data.current.uvi);
+
 
             })
 
+            return;
+
         } else {
             alert("Something went wrong with api");
+            return;
         }
     })
 }
