8d3f368dc8c503e89a884bc0a3c2114d24da1dc7
Joona Lusa <joona.lusa@hotmail.com>
2021-11-25 11:39:27 -0500

    current weather is shown to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,24 @@
 const empty = "";
+const low = 3;
+const moderate = 8;
+const high = 11;
+var todaysDate = moment().toDate();
+todaysDate = moment(todaysDate).format("DD/MM/YYYY");
+var currentWeather = document.querySelector("#current");
 var searchList = document.querySelector(".search-history");
 
 $("#search-btn").click(function() {
 
     var searchCity = $("#user-search").val();
-    // API key: A2mD7VganlN2FLWzfesMPmEdQm8etbxpl9ApyKhFC_k
-    var geoLocationUrl = "https://geocode.search.hereapi.com/v1/geocode?q=" + searchCity + "&apiKey=A2mD7VganlN2FLWzfesMPmEdQm8etbxpl9ApyKhFC_k";
+    // API key: mj4KYckuw8lalUVDqTRMUGr_21lA1l9lyXnpdHRvl7o
+    var geoLocationUrl = "https://geocode.search.hereapi.com/v1/geocode?q=" + searchCity + "&apiKey=mj4KYckuw8lalUVDqTRMUGr_21lA1l9lyXnpdHRvl7o";
 
     if (searchCity !== "") {
         var newBtnEl = document.createElement("button");
         newBtnEl.classList = "btn-primary btn-sm btn-block list-group-item-dark mb-2";
         newBtnEl.textContent = searchCity;
         searchList.appendChild(newBtnEl);
-        $(searchCity).val(empty);
+        $("#user-search").val(empty);
         fetch(geoLocationUrl).then(function(response) {
             if (response.ok) {
 
@@ -41,14 +47,8 @@ var getCurrentWeather = function(search, data) {
         if (response.ok) {
 
             response.json().then(function(data) {
-                console.log(data.current);
-                console.log(search);
-                console.log(data.current.temp);
-                console.log(data.current.wind_speed);
-                console.log(data.current.humidity);
-                console.log(data.current.uvi);
 
-
+                addCurrWeathertoSite(search, data);
             })
 
             return;
@@ -58,4 +58,43 @@ var getCurrentWeather = function(search, data) {
             return;
         }
     })
+}
+
+var addCurrWeathertoSite = function(city, data) {
+    var headerEl = document.createElement("h3");
+    headerEl.textContent = city + " (" + todaysDate + ")";
+    currentWeather.appendChild(headerEl);
+
+    var tempEL = document.createElement("p");
+    tempEL.textContent = "Temp: " + data.current.temp + " ℃";
+    currentWeather.appendChild(tempEL);
+
+    var windEl = document.createElement("p");
+    windEl.textContent = "Wind: " + data.current.wind_speed + " KMH";
+    currentWeather.appendChild(windEl);
+
+    var humEl = document.createElement("p");
+    humEl.textContent = "Humidity: " + data.current.humidity + " %";
+    currentWeather.appendChild(humEl);
+
+    var uviEl = document.createElement("p");
+
+    uviEl.textContent = "UV Index: ";
+    var spanEl = document.createElement("span");
+    if (data.current.uvi >= 0 && data.current.uvi < low) {
+        spanEl.setAttribute("style", "color: white; padding: 5px; background-color: green");
+        spanEl.textContent = data.current.uvi;
+    } else if (data.current.uvi >= low && data.current.uvi < moderate) {
+        spanEl.setAttribute("style", "color: white; padding: 5px; background-color: orange");
+        spanEl.textContent = data.current.uvi;
+    } else if (data.current.uvi >= moderate && data.current.uvi < high) {
+        spanEl.setAttribute("style", "color: white; padding: 5px; background-color: red");
+        spanEl.textContent = data.current.uvi;
+    }
+
+    uviEl.appendChild(spanEl);
+    currentWeather.appendChild(uviEl);
+
+
+
 }
